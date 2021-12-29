152d5c55deb93cd675488dd6a009d4260e7f5eae
neda-ara <neda.ara9@gmail.com>
2021-12-29 21:58:23 +0530

    Linear gradient

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import { StyleSheet, Text, View, ImageBackground, Image } from "react-native";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
+import { LinearGradient } from 'expo-linear-gradient';
 
 const fetchFonts = () => {
   return Font.loadAsync({
     "DM-serif-regular": require("./assets/fonts/DMSerifDisplay-Regular.ttf"),
+    "DeathNote": require("./assets/fonts/DEATHNOTEFont.ttf"),
   });
 };
 
@@ -24,12 +26,21 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={require("./assets/Images/purple_bg.jpg")}
-        style={styles.backgroungImg}
-      >
-        <Text style={styles.welcomeText}>WELCOME TO MORSEL</Text>
-      </ImageBackground>
+          <ImageBackground
+            source={require("./assets/Images/purple_bg.jpg")}
+            style={styles.backgroungImg}
+          >
+            <LinearGradient
+        // Background Linear Gradient
+        colors={["rgba(0, 0, 0, 0.79)", "rgba(0, 0, 0, 0.49)", "rgba(0, 0, 0, 0.69)", "rgba(0, 0, 0, 0.89)"]}
+        style={styles.background}
+        >
+            <View style={styles.backgroundView}>
+              <Text style={styles.welcomeText}>MORSEL</Text>
+            </View>
+            
+        </LinearGradient>
+          </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -50,9 +61,23 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  welcomeText: {
-    fontSize: 28,
-    color: "#fff",
-    fontFamily: "DM-serif-regular",
+  backgroundView: {
+    width: "100%",
+    height: "100%",
+    marginRight: 150,
+    marginLeft: 150,
+    justifyContent: "center",
+    alignItems: "center",
   },
+  welcomeText: {
+    fontSize: 48,
+    color: "#fff",
+    fontFamily: "DeathNote",
+  },
+  Logo: {
+    width: 60,
+    height: 60,
+    marginHorizontal: 20,
+    marginLeft: 25
+  }
 });
