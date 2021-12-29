f23e417fc2ef25200c21d6757d115811ef333504
neda-ara <neda.ara9@gmail.com>
2021-12-29 20:32:02 +0530

    Welcome Screen

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "DM-serif-regular": require("./assets/fonts/DMSerifDisplay-Regular.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground
+        source={require("./assets/Images/purple_bg.jpg")}
+        style={styles.backgroungImg}
+      >
+        <Text style={styles.welcomeText}>WELCOME TO MORSEL</Text>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +38,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    marginTop: 30,
+  },
+  backgroungImg: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  welcomeText: {
+    fontSize: 28,
+    color: "#fff",
+    fontFamily: "DM-serif-regular",
   },
 });
